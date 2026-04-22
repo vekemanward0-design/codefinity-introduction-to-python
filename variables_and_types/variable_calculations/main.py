2ef562101ad3da0a65b771590e6a19c1219e11c5
vekemanward0-design <>
2026-04-22 11:16:32 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,9 @@
+item_name = "Soda" # Assign a string for the item name
+item_price = 6.99  # Assign a float for the price per unit 
+purchase_quantity = 5  # Assign an integer for the quantity being purchased
+
+# Use the appropriate variables to calculate the total cost for 5 cases of soda at $6.99 per case
+total_cost = item_price * purchase_quantity
+
+# Testing
+print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
