79bded78742f0c3dfd7b700652404d2897f75115
vekemanward0-design <>
2026-04-22 11:12:31 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,8 @@
+item_name = "Bread" # Create a variable name that clearly represents it's assignment of "bread"
+item_price = 4.52  # Create a variable name that clearly represents it's assignment of $4.52 for the price of bread
+items_in_stock = 230  # Create a variable name that clearly represents it's assignment of 230 loafs of bread in stock
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", items_in_stock)
